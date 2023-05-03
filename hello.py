38b601cb28f810c3c021cacf75b05d70858821a4
Roman Deriga <deriga.roman@gmail.com>
2023-05-03 11:16:37 +0700

    first commit

---
+++ hello.py
@@ -0,0 +1,79 @@
+from time import sleep
+
+print('\u001b[32;1m', end="")
+print ("""                _-o#&&*''''?d:>b\\_
+              _o/'`''  '',, dMF9MMMMMHo_
+           .o&#'        `'MbHMMMMMMMMMMMHo.
+         .o'' '         vodM*$&&HMMMMMMMMMM?.
+        ,'              $M&ood,~'`(&##MMMMMMH\\
+       /               ,MMMMMMM#b?#bobMMMMHMMML
+      &              ?MMMMMMMMMMMMMMMMM7MMM$R*Hk
+     ?$.            :MMMMMMMMMMMMMMMMMMM/HMMM|`*L
+    |               |MMMMMMMMMMMMMMMMMMMMbMH'   T,
+    $H#:            `*MMMMMMMMMMMMMMMMMMMMb#}'  `?
+    ]MMH#             ''*''''*#MMMMMMMMMMMMM'    -
+    MMMMMb_                   |MMMMMMMMMMMP'     :
+    HMMMMMMMHo                 `MMMMMMMMMT       .
+    ?MMMMMMMMP                  9MMMMMMMM}       -
+    -?MMMMMMM                  |MMMMMMMMM?,d-    '
+     :|MMMMMM-                 `MMMMMMMT .M|.   :
+      .9MMM[                    &MMMMM*' `'    .
+       :9MMk                    `MMM#'        -
+         &M}                     `          .-
+          `&.                             .
+            `~,   .                     ./
+                . _                  .-PP
+                  '`--._,dd###pp='''""")
+print('\033[2J', end="")
+print('\u001b[45A',end="")
+sleep(1)
+print ("""                  .ovr:HMM#?:`' >b\\_
+              .,:&Hi' `'   '' \\\\|&bSMHo_
+            oHMMM#*}          `?&dMMMMMMHo.
+         .dMMMH'''''           ,oHH*&&9MMMM?.
+        ,MMM*'                 `*M\\bd<|'*&#MH\\
+       dHH?'                   :MMMMMM#bd#odMML
+      H' |\\                  `dMMMMMMMMMMMMMM9Mk
+     JL/'7+,.                `MMMMMMMMMMMMMMMH9ML
+    -`Hp     '               |MMMMMMMMMMMMMMMMHH|:
+    :  \\\\#M#d?                `HMMMMMMMMMMMMMMMMH.
+    .   JMMMMM##,              ``*''''*#MMMMMMMMH
+    -. ,MMMMMMMM6o_                    |MMMMMMMM':
+    :  |MMMMMMMMMMMMMb\\                 TMMMMMMT :
+    .   ?MMMMMMMMMMMMM'                 :MMMMMM|.`
+    -    ?HMMMMMMMMMM:                  HMMMMMM\\|:
+     :     9MMMMMMMMH'                 `MMMMMP.P.
+      .    `MMMMMMT''                   HMMM*''-
+       -    TMMMMM'                     MM*'  -
+        '.   HMM#                            -
+          -. `9M:                          .'
+            -. `b,,    .                . '
+              '-\\   .,               .-`
+                  '-:b~\\\\_,oddq==--'""")
+#slide 3
+print('\033[2J', end="")
+print('\u001b[45A',end="")
+sleep(1)
+print ("""                  _oo##'9MMHb':'-,o_
+              .vM':HH$'    '''  '' -\\7*_
+           .oHMMMHMH#9:          '\\bMMMMHo.
+         . ,dMMMMMMMMMMM'`' `           ?MP?.
+        . |MMMMMMMMMMM'                 `'$b&\\
+       -  |MMMMHH##M'                     HMMH?
+      -   TTMM|    >..                   \\MMMMMH
+     :     |MM\\,#-''$~b\\.                `MMMMMM+
+    .       ``'H&#        -               &MMMMMM|
+    :            *\\v,#MHddc.              `9MMMMMb
+    .               MMMMMMMM##\\             `'':HM
+    -          .  .HMMMMMMMMMMRo_.              |M
+    :             |MMMMMMMMMMMMMMMM#\\           :M
+    -              `HMMMMMMMMMMMMMMM'           |T
+    :               `*HMMMMMMMMMMMM'            H'
+     :                 MMMMMMMMMMM|            |T
+      .                MMMMMMMM?'             ./
+      `.               MMMMMMH'              ./
+        -.            |MMMH#'                .
+          .           `MM*                . '
+            -.         #M: .    .       .-
+              ` .         .,         .-
+                  '-.-~ooHH__,,v~--`""")
